fe5607bd4fc09541558b67f8151190dea1e15dda
Nicolas Montecchiani <montexdev@protonmail.com>
2024-05-07 23:36:34 +0200

    I'm really tired

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,6 @@ export default async function HomePage() {
   return (
     <main className="flex flex-col">
       <DisplaySearchMultiple />
-      <code className="">User: {JSON.stringify(user, null, 2)}</code>
-      <code>Result: {JSON.stringify(result, null, 2)}</code>
     </main>
   );
 }
