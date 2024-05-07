7ab6ee0765c4db0dab5c68c6115f0942a1359a1a
Nicolas Montecchiani <montexdev@protonmail.com>
2024-05-07 23:21:02 +0200

    Layout issue

---
+++ src/app/(root)/detail/_components/Search.tsx
@@ -0,0 +1,21 @@
+import { redirect } from "next/navigation";
+
+export function DisplaySearchMultiple() {
+  async function searchMovieSeries(formData: FormData) {
+    "use server";
+    const search = formData.get("search") as string;
+
+    redirect(`/search/${search}`);
+  }
+
+  return (
+    <form action={searchMovieSeries} className="mb-3 mt-2 flex justify-center">
+      <input
+        type="text"
+        name="search"
+        placeholder={"Search a TV Series a Movie or a Person"}
+        className="w-96 rounded-full px-4 py-2 text-lg text-black"
+      />
+    </form>
+  );
+}
--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,10 +1,7 @@
 import "~/styles/globals.css";
 
 import { Inter } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
-import { redirect } from "next/navigation";
-import { getUser } from "~/lib/auth";
+import { DisplaySearchMultiple } from "./detail/_components/Search";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -16,26 +13,6 @@ export const metadata = {
   description: "The place to track all your favorite series and movies",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
-
-export function DisplaySearchMultiple() {
-  async function searchMovieSeries(formData: FormData) {
-    "use server";
-    const search = formData.get("search") as string;
-
-    redirect(`/search/${search}`);
-  }
-
-  return (
-    <form action={searchMovieSeries} className="mb-3 mt-2 flex justify-center">
-      <input
-        type="text"
-        name="search"
-        placeholder={"Search a TV Series a Movie or a Person"}
-        className="w-96 rounded-full px-4 py-2 text-lg text-black"
-      />
-    </form>
-  );
-}
 
 export default async function AppLayout({
   children,
