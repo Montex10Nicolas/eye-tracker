d4b626bd5baec94ad50de486930b28d5d4bdd8fb
Nicolas Montecchiani <montexdev@protonmail.com>
2024-06-18 23:13:39 +0200

    rebase

--- src/app/(root)/detail/tv/[tv_id]/_components/Client.tsx
+++ src/app/(root)/detail/tv/[tv_id]/_components/Client.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Link from "next/link";
 import { useState } from "react";
 import { TMDB_IMAGE_URL } from "~/_utils/utils";
 import { type Cast, type Credits, type Crew } from "~/types/tmdb_detail";
